239baf661c8625b239a18bdb21db80312f24e5e7
Richard <gr40in@yandex.ru>
2024-05-27 09:40:19 +0500

    Initial commit

---
+++ src/main/java/org/richard/htmlcsshw06/HtmlCssHw06Application.java
@@ -0,0 +1,13 @@
+package org.richard.htmlcsshw06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlCssHw06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmlCssHw06Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/richard/htmlcsshw06/HtmlCssHw06ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.richard.htmlcsshw06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlCssHw06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
